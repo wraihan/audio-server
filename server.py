3eac4a88b4f31d815b8d43e35691a68466da0533
wraihan <raihan.sulong@gmail.com>
2025-07-17 15:33:13 +0800

    updating server Thu Jul 17 03:33:13 PM +08 2025

--- server.py
+++ server.py
@@ -1,26 +1,22 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, url_for, send_from_directory
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    base_path = os.path.join('audio')
+    base_path = os.path.join('media')
     folders = sorted([f for f in os.listdir(base_path) if os.path.isdir(os.path.join(base_path, f))])
     return render_template('index.html', folders=folders)
 
 @app.route('/folder/<folder>')
 def show_folder(folder):
-    folder_path = os.path.join('audio', folder)
+    folder_path = os.path.join('media', folder)
     if not os.path.exists(folder_path):
         return "Folder not found", 404
     songs = sorted([f for f in os.listdir(folder_path) if f.endswith('.mp3')])
     return render_template('folder.html', folder=folder, songs=songs)
-
-@app.route('/audio/<folder>/<filename>')
-def serve_song(folder, filename):
-    return send_from_directory(os.path.join('audio', folder), filename)
 
 
 if __name__ == '__main__':
